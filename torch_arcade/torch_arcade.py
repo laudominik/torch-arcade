6dd9fcbdedb90c932794c52e05e42948f7700f37
Dominik Lau <harek2388@gmail.com>
2025-04-06 14:09:47 +0200

    added ARCADESemanticSegmentation

--- torch_arcade/torch_arcade.py
+++ torch_arcade/torch_arcade.py
@@ -13,6 +13,8 @@ from pycocotools import mask as coco_mask
 from torchvision.datasets.utils import download_and_extract_archive
 from torchvision.datasets import VisionDataset
 
+from encoding import ENCODING, COLOR_DICT
+
 class _ARCADEBase(VisionDataset):
     URL="https://zenodo.org/records/8386059/files/arcade_challenge_datasets.zip"
     ZIPNAME="arcade_challenge_datasets.zip"
@@ -80,6 +82,31 @@ class _ARCADEBase(VisionDataset):
     def __len__(self) -> int:
         return len(self.images)
 
+
+def cached_mask(coco, cache_dir, 
+                img_filename, img_id, 
+                reduce):
+    img = Image.open(img_filename)
+    mask_file = os.path.join(
+        cache_dir,
+        f"{os.path.basename(img_filename).replace('.png', '.npz')}"
+    )
+    categories = coco.loadCats(coco.getCatIds())
+
+    if os.path.exists(mask_file):
+        mask = np.load(mask_file)["mask"]
+    else:
+        annotations = coco.loadAnns(coco.getAnnIds(imgIds=img_id))
+        width, height = img.size
+        mask = np.zeros((height, width), dtype=np.uint8)
+        mask = None
+        for ann in annotations:
+            category = categories[ann["category_id"]]
+            mask = reduce(mask, coco.annToMask(ann), category)
+        # np.savez_compressed(mask_file, mask=mask)
+    return mask
+
+
 class ARCADEBinarySegmentation(_ARCADEBase):
     TASK = "segmentation"
     MASK_CACHE = "masks"
@@ -97,52 +124,78 @@ class ARCADEBinarySegmentation(_ARCADEBase):
         self.mask_dir = os.path.join(self.dataset_dir, ARCADEBinarySegmentation.MASK_CACHE)        
         os.makedirs(self.mask_dir, exist_ok=True)
 
-    def __getitem__(self, index: int) -> Tuple[Any, Any]:
-        """
-        Args:
-            index (int): Index
+    @staticmethod
+    def reduction(mask, other, _):
+        if mask is None: other
+        return mask | other
 
-        Returns:
-            tuple: (image, target) where target is the image segmentation.
-        """
+    def __getitem__(self, index: int) -> Tuple[Any, Any]:
         img_filename = self.images[index]
         img = Image.open(img_filename)
         id = self.file_to_id[img_filename]
-
-        mask_file = os.path.join(
-            self.mask_dir,
-            f"{os.path.basename(img_filename).replace('.png', '.npz')}"
-        )
-
-        mask = None
-        if os.path.exists(mask_file):
-            mask = np.load(mask_file)["mask"]
-        else:
-            annotations = self.coco.loadAnns(self.coco.getAnnIds(imgIds=id))
-            width, height = img.size
-            mask = np.zeros((height, width), dtype=np.uint8)
-            mask = self.coco.annToMask(annotations[0])
-            for ann in annotations:
-                mask |= self.coco.annToMask(ann)
-            np.savez_compressed(mask_file, mask=mask)
+        mask =  cached_mask(self.coco, self.mask_dir, img_filename, id, ARCADEBinarySegmentation.reduction)
         return img, mask
 
-class ARCADESemanticSegmentation():
-    pass
+
+class ARCADESemanticSegmentation(_ARCADEBase):
+    TASK = "segmentation"
+    MASK_CACHE = "masks_semantic"
+    CAT_TO_ONEHOT = {}
+
+    def __init__(
+        self,
+        root: Union[str, Path],
+        image_set: str = "train",
+        download: bool = False,
+        transform: Optional[Callable] = None,
+        target_transform: Optional[Callable] = None,
+        transforms: Optional[Callable] = None,
+    ):
+        super().__init__(root, image_set, ARCADESemanticSegmentation.TASK, download, transform, target_transform, transforms)
+        self.mask_dir = os.path.join(self.dataset_dir, ARCADESemanticSegmentation.MASK_CACHE)                
+        os.makedirs(self.mask_dir, exist_ok=True)
+
+    @staticmethod
+    def reduction(mask, other, other_cat):
+        one_hot_vector = np.array(ENCODING[other_cat['name']])
+        width, height = other.shape
+        other_oh = np.zeros((width, height, len(one_hot_vector)))
+        other_oh[(other == 1)] = one_hot_vector
+
+        if mask is None: return other_oh
+        return np.maximum(mask, other_oh)
+
+    def __getitem__(self, index: int) -> Tuple[Any, Any]:
+        img_filename = self.images[index]
+        img = Image.open(img_filename)
+        id = self.file_to_id[img_filename]
+        mask =  cached_mask(self.coco, self.mask_dir, img_filename, id, ARCADESemanticSegmentation.reduction)
+        return img, mask
+
 
 class ARCADEStenosisDetection():
     pass
 
 
+def onehot_to_rgb(onehot, color_dict):
+    single_layer = np.argmax(onehot, axis=-1)
+    width, height, _ = onehot.shape
+    output = np.zeros( (width, height, 3) )
+    # print(single_layer)
+    # # print(output[single_layer==1].shape)
+    for k in range(len(color_dict)):
+        output[single_layer==k] = np.array(color_dict[k])
+    return np.uint8(output)
+
 if __name__ == "__main__":
     import matplotlib.pyplot as plt
-    ds = ARCADEBinarySegmentation(
+    ds = ARCADESemanticSegmentation(
         "dataset/",
         image_set="train",
         download="true"
     )
-    img, mask = ds[0]
-    plt.imshow(img)
-    plt.show()
-    plt.imshow(mask)
-    plt.show()
+    for img, mask in ds:  
+        plt.imshow(img)
+        plt.show()
+        plt.imshow(onehot_to_rgb(mask, COLOR_DICT))
+        plt.show()
