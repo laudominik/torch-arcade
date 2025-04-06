831795b554322e8a886ae7f65f88a8e0627bc17a
Dominik Lau <harek2388@gmail.com>
2025-04-06 12:07:05 +0200

    ARCADE base

--- torch_arcade/torch_arcade.py
+++ torch_arcade/torch_arcade.py
@@ -9,19 +9,56 @@ from PIL import Image
 from torchvision.datasets.utils import download_and_extract_archive
 from torchvision.datasets import VisionDataset
 
+DATASET_DICT = {
+    "segmentation" : {
+        "train": {
+            "path": os.path.join("dataset_phase_1", "segmentation_dataset", "seg_train"),
+            "coco": "seg_train.json",
+        },
+        "val": {
+            "path": os.path.join("dataset_phase_1", "segmentation_dataset", "seg_val"),
+            "coco": "seg_val.json"
+        },
+        "test": {
+            "path": os.path.join("dataset_final_phase", "test_case_segmentation"),
+            "coco": "instances_default.json"
+        }
+    },
+    "stenosis" : {
+        "train": {
+            "path": os.path.join("dataset_phase_1", "stenosis_dataset", "sten_train"),
+            "coco": "sten_train.json"
+        },
+        "val": {
+            "path": os.path.join("dataset_phase_1", "stenosis_dataset", "sten_val"),
+            "coco": "sten_val.json"
+        },
+        "test": {
+            "path": os.path.join("dataset_final_phase", "test_cases_stenosis"),
+            "coco": "instances_default.json"
+        }
+    },
+}
+
+
 class _ARCADEBase(VisionDataset):
-    """ARCADE <https://zenodo.org/records/8386059> segmentation/stenosis detection dataset
+    """`ARCADE <https://zenodo.org/records/8386059>`_ segmentation/stenosis detection dataset
         root (str or ``pathlib.Path``): Root directory of the ARCADE dataset.
         phase (string, optional): Which phase to use, supports ``"phase_1"``, ``"final_phase"``
+        image_set (string, optional): One of ``"train"``, ``"val"``, ``"test"``
+        
+        note: requires pycocoutils for target loading
     """
 
     URL="https://zenodo.org/records/8386059/files/arcade_challenge_datasets.zip"
-    FILENAME="arcade_challenge_datasets.zip"
+    ZIPNAME="arcade_challenge_datasets.zip"
+    FILENAME="arcade_challenge_datasets"
 
     def __init__(
         self,
         root: Union[str, Path],
-        phase: str = "phase_1",
+        image_set: str = "train",
+        task: str = "segmentation",
         download: bool = False,
         transform: Optional[Callable] = None,
         target_transform: Optional[Callable] = None,
@@ -29,11 +66,19 @@ class _ARCADEBase(VisionDataset):
     ):
         super().__init__(root, transforms, transform, target_transform)
 
-        self.phase = phase
         if download:
-            download_and_extract_archive(_ARCADEBase.URL, self.root, filename=_ARCADEBase.FILENAME)
+            download_and_extract_archive(_ARCADEBase.URL, self.root, filename=_ARCADEBase.ZIPNAME)
 
-
+        task_dict = DATASET_DICT[task][image_set]
+        self.dataset_dir = os.path.join(self.root,  _ARCADEBase.FILENAME, task_dict["path"])
+        self.coco = os.path.join(self.dataset_dir, "annotations", task_dict["coco"])
+        
+        image_dir = os.path.join(self.dataset_dir, "images")
+        self.images = [
+            os.path.join(image_dir, file_name) 
+            for file_name in os.listdir(image_dir) 
+            if file_name.endswith('.png')
+        ]
 
     def __len__(self) -> int:
         return len(self.images)
@@ -49,7 +94,9 @@ class ARCADEStenosisDetection():
 
 
 if __name__ == "__main__":
-    _ARCADEBase(
+    ds = _ARCADEBase(
         "dataset/",
+        image_set="train",
         download="true"
     )
+    print(len(ds))
